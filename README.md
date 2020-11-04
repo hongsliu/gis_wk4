3469944b0dc66d1a14e6f7b7be6407a97ad69476
Hongsheng Liu <hongs.liu@outlook.com>
2020-11-04 16:05:17 +0800

    third

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gis_wk4
+<!-- badges: start -->
+[![Launch Rstudio Binder](http://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/hongsliu/gis_wk4/master?urlpath=rstudio)
+<!-- badges: end -->
---
+++ binder_test.R
@@ -0,0 +1,7 @@
+library(holepunch)
+write_compendium_description(package = "wk4_binder", 
+                             description = "test")
+
+write_dockerfile(maintainer = "hongsheng") 
+
+generate_badge()
